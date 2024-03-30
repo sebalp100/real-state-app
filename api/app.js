4c10a3d8497aba6e37b08fda0fcb6059e64cd00c
sebalp100 <sebalp100@gmail.com>
2024-03-30 20:36:56 -0300

    Add prisma schema

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.route.js'
 import authRoute from './routes/auth.route.js'
+import 'dotenv/config'
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,25 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      }
+    })
 
-  res.send(hashedPassword);
+    res.status(201).json({ message: "User created!" });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create user" });
+    console.log(err);
+  }
+
 
 };
 
