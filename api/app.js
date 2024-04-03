aded2f1f07acf7a1e2e1c10b4f0fa513c64ab92a
sebalp100 <sebalp100@gmail.com>
2024-04-03 16:15:49 -0300

    Add front-end files and update cors config

--- api/app.js
+++ api/app.js
@@ -3,12 +3,16 @@ import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.route.js'
 import authRoute from './routes/auth.route.js'
 import 'dotenv/config'
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
---
+++ client/src/lib/apiRequest.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const apiRequest = axios.create({
+  baseURL: import.meta.env.VITE_AXIOS_URL,
+  withCredentials: true
+});
+
+export default apiRequest;
