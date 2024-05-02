30179aaf674ede94655b1cc7a94c0232a3525d69
sebalp100 <sebalp100@gmail.com>
2024-05-02 00:06:21 -0300

    Add global context for user

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import apiRequest from '../../lib/apiRequest';
 import './login.scss';
 import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 
 function Login() {
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const { updateUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
@@ -23,8 +25,7 @@ function Login() {
         password,
       });
 
-      console.log(res);
-      localStorage.setItem('user', JSON.stringify(res.data));
+      updateUser(res.data);
 
       navigate('/');
     } catch (err) {
--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -3,12 +3,17 @@ import Chat from '../../components/chat/Chat';
 import List from '../../components/list/List';
 import apiRequest from '../../lib/apiRequest';
 import './profilePage.scss';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 function ProfilePage() {
   const navigate = useNavigate();
+  const { currentUser, updateUser } = useContext(AuthContext);
+
   const handleLogout = async () => {
     try {
-      const res = apiRequest.post('/auth/logout');
+      await apiRequest.post('/auth/logout');
+      updateUser(null);
       navigate('/');
 
       console.log('Logout successful');
@@ -16,6 +21,7 @@ function ProfilePage() {
       console.log(err);
     }
   };
+
   return (
     <div className="profilePage">
       <div className="details">
@@ -27,16 +33,13 @@ function ProfilePage() {
           <div className="info">
             <span>
               Avatar:
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
+              <img src={currentUser.avatar || '/noavatar.jpg'} alt="avatar" />
             </span>
             <span>
-              Username: <b>John Doe</b>
+              Username: <b>{currentUser.username}</b>
             </span>
             <span>
-              E-mail: <b>john@gmail.com</b>
+              E-mail: <b>{currentUser.email}</b>
             </span>
             <button onClick={handleLogout}>Logout</button>
           </div>
