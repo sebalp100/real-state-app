b52843c9b5e76ec1a5c5d1af8ff109361f249026
sebalp100 <sebalp100@gmail.com>
2024-05-09 21:11:08 -0300

    Add create post page and loader

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -59,8 +59,9 @@ export const getPost = async (req, res) => {
           res.status(200).json({ ...post, isSaved: saved ? true : false });
         }
       });
+    } else {
+      res.status(200).json({ ...post, isSaved: false });
     }
-    res.status(200).json({ ...post, isSaved: false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
